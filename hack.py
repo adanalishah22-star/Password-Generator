de56561353c868de2c53123becf5db03369802f5
Adan Ali <adanalishah22@gmail.com>
2026-05-09 18:55:30 +0500

    Fix: Added missing colon in function definition

--- hack.py
+++ hack.py
@@ -1,6 +1,8 @@
-name = input("Enter Name: ")
-password = input("Enter Password: ")
-if password == "0099":
-    print("Welcome Sir", name, "🔥")
-else:
-    print("Incorrect Password")
+def login_system() :
+    name = input("Enter Name: ")
+    password = input("Enter Password: ")
+    if password == "0099":
+       print("Welcome Sir", name, "🔥")
+    else:
+       print("Incorrect Password")
+login_system()
